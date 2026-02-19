da06bb6b39f2e3c05520d7f01ad92abecd4d3f1b
Jeffrey Lee <jeffreyleee3@gmail.com>
2026-02-18 17:38:02 -0800

    init commit

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,15 @@
+interface BlogPostProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function BlogPost({ params }: BlogPostProps) {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <h1 className="text-4xl font-bold">Blog Post: {params.slug}</h1>
+      </div>
+    </main>
+  );
+}
